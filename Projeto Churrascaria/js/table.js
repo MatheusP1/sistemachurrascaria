18c214aef9486ab4e12cf985c37e1313bddccc89
Matheus Pereira <matheusspereira0@gmail.com>
2018-11-07 15:55:49 -0200

    add icones, tabela e ajuste de layout

--- Projeto Churrascaria/js/table.js
+++ Projeto Churrascaria/js/table.js
@@ -1,5 +1,21 @@
-function inserirLinha(){
 
+var agua = 2.00;
+var snatural = 8.00;
+var slata = 5.00;
+var rlata = 5.00;
+var rgarrafa = 8.00;
+var cerveja = 6.00;
+var sake = 14.00;
+var cachaca = 12.00;
+var pacoca = 1.00;
+var tortinha = 2.00;
+var paleta = 8.00;
+var bolo = 6.00;
+var sorvete = 15.00;
+
+
+
+function inserirLinha(nome, valor){
     //indentificacao da tabela
     var table = document.getElementById("tabela");
     //declaracao da linha
@@ -8,8 +24,8 @@ function inserirLinha(){
     var campoProduto = document.createElement("td");
     var campoPreco = document.createElement("td");
     //declaracao dos atributos
-    var txtProduto = document.createTextNode('nome');
-    var txtPreco = document.createTextNode('2.00');
+    var txtProduto = document.createTextNode(nome);
+    var txtPreco = document.createTextNode(valor);
     //add os atributos aos campos
     campoProduto.appendChild(txtProduto);
     campoPreco.appendChild(txtPreco);
